cfa39cd8dd959771e4f0383b6895e033c469bfca
Sasha Bezverkha <sasha.bezverha@gmail.com>
2016-04-18 15:41:26 +0300

    fix move

--- robot.js
+++ robot.js
@@ -4,60 +4,84 @@ $(document).ready(function() {
             //left = 37,
             //up = 38,
             //right = 39,
-            //down = 40
+            //down = 40///////////
+        /////////////////////////////table
+        table_top=$("table").offset().top;
+        table_left=$("table").offset().left;
+        height=$("table").offset().top +$("table").height()-$('.robot').height();
+        width=$("table").offset().left +$("table").width()-$('.robot').width();
+      ///////////////////////////robot
         robot_top=$(".robot").offset().top;
         robot_left=$(".robot").offset().left;
-        park1_top=$(".park1").offset().top;
-        park3_top=$(".park3").offset().top;
-        park1_left=$(".park1").offset().left;
-        walk4_top=$(".walk4").offset().top;
-        walk3_top=$(".walk3").offset().top;
-        walk1_left=$(".walk1").offset().left;
-        walk2_left=$(".walk2").offset().left;
-        walk2_top=$(".walk2").offset().top;
-        cross1_1_left=$(".cross1").offset().left;
-        cross1_2_left=$(".cross1").offset().left+$(".cross1").width()-$('.robot').width();
-        //cross2_1_top=$(".cross2_1").offset().top;
-        //cross2_1_left=$(".cross2_1").offset().left;
-        //cross2_2_top=$(".cross2_2").offset().top;
-        zone1_top=park1_top+$('.park1').height()+$('.walk3').height()-$('.robot').height();
-        zone1_left=park1_left+$('.park1').width()+$('.walk1').width()-$('.robot').width();
-        zone2_top=walk4_top-$('.robot').height();
-        zone3_top=$(".walk2").offset().top+$('.robot').height();
-        height=$('table').height()-$('.robot').height();
-        width=$('table').width()-$('.robot').width();
 
-        zone1_height=park1_top+$('.walk3').height()+$('.park1').height()-$('.robot').height();
-        zone1_width=park1_left+$('.walk1').width()+$('.park1').width()-$('.robot').width();
+//////////////////////////////////////////////////////////////////////road1
+        road1_top=$(".road1").offset().top-$('.robot').height();
+        road1_height=$(".road1").offset().top+$('.road1').height();
+        road1_left=$(".road1").offset().left;
+        //////////////////////////////////////////////////////////////road2
+        road2_left=$(".road2").offset().left-$('.robot').width();
+        road2_width=$(".road2").offset().left+$('.road2').width();
+/////////////////////////////////////////////cross1
+        cross1_top=$(".cross1").offset().top;
+        cross1_width=$(".cross1").offset().left+$(".cross1").width()-$('.robot').width();
+        cross1_left=$(".cross1").offset().left;
+///////////////////////////////////////////////cross2
+        cross2_top=$(".cross2").offset().top;
+        cross2_width=$(".cross2").offset().left+$(".cross2").width()-$('.robot').width();
+        cross2_left=$(".cross2").offset().left;
+/////////////////////////////////////////////cross3
+        cross3_top=$(".cross3").offset().top;
+        cross3_height=$(".cross3").offset().top+$(".cross3").height()-$('.robot').height();
+
+///////////////////////////////////////////////cross4
+        cross4_top=$(".cross4").offset().top;
+        cross4_height=$(".cross4").offset().top+$(".cross4").height()-$('.robot').height();
+
+
+
+
 
 //////down
-        if (e.keyCode == 40&&robot_top<zone1_height||e.keyCode == 40&&robot_top<height&&robot_top>=walk4_top) {
+        if (e.keyCode == 40&&robot_top<road1_top&&(robot_left<=road2_left||robot_left>=road2_width)) {
             $(".robot").css("top",  $(".robot").offset().top + 10); }
-        if (e.keyCode == 40&&robot_top>= zone1_height&&robot_left>=cross1_1_left&&robot_left<=cross1_2_left&&robot_top<height&&(green.css("background-color")=="rgb(43, 205, 30)"||green.css("background-color")=="rgb(175, 250, 154)")) {
-            $(".robot").css("top",  $(".robot").offset().top+ 10); }
-        if (e.keyCode == 40&&robot_top> zone1_height&&robot_left>=cross1_1_left&&robot_left<=cross1_2_left&&robot_top<height&&(green.css("background-color")!="rgb(43, 205, 30)"||green.css("background-color")!="rgb(175, 250, 154)")) {
-            $(".robot").css("top",  $(".robot").offset().top+ 10); }
+        if (e.keyCode == 40&&robot_top>=road1_top&&((robot_left>=cross1_left&&robot_left<=cross1_width)||robot_left>=cross2_left&&robot_left<=cross2_width)&&robot_top<road1_height&&(green.css("background-color")=="rgb(43, 205, 30)"||green.css("background-color")=="rgb(175, 250, 154)")) {
+            $(".robot").css("top",  $(".robot").offset().top + 10); }
+        if (e.keyCode == 40&&robot_left>=road2_left&&robot_left<=road2_width&&(robot_top>=cross3_top&&robot_top<cross3_height||robot_top>=cross4_top&&robot_top<cross4_height)) {
+            $(".robot").css("top",  $(".robot").offset().top + 10); }
+        if (e.keyCode == 40&&robot_top>=road1_height&&(robot_left<=road2_left||robot_left>=road2_width)&&robot_top<height) {
+            $(".robot").css("top",  $(".robot").offset().top + 10); }
 
-///////right
-        if (e.keyCode == 39&&robot_left<zone1_width&&robot_top<=zone1_height||e.keyCode == 39&&robot_left<zone1_width&&robot_top>=walk4_top) {
+
+
+/////right
+        if (e.keyCode == 39&&(robot_left<road2_left||robot_left>=road2_width)&&robot_top<=road1_top&&robot_left<width) {
             $(".robot").css("left",  $(".robot").offset().left + 10);}
-        if (e.keyCode == 39&&robot_top>=zone1_height&&robot_top<=walk4_top&&robot_left>=cross1_1_left&&robot_left<cross1_2_left) {
-            $(".robot").css("left",  $(".robot").offset().left+ 10);}
+        if (e.keyCode == 39&&robot_left>=road2_left&&robot_left<=road2_width&&(robot_top>=cross3_top&&robot_top<=cross3_height||robot_top>=cross4_top&&robot_top<=cross4_height)) {
+            $(".robot").css("left",  $(".robot").offset().left + 10);}
+        if (e.keyCode == 39&&robot_top<=road1_height&&((robot_left>=cross1_left&&robot_left<cross1_width)||robot_left>=cross2_left&&robot_left<cross2_width)&&robot_top>road1_top) {
+            $(".robot").css("left",  $(".robot").offset().left + 10);}
+        if (e.keyCode == 39&&(robot_left<road2_left||robot_left>=road2_width)&&robot_top>=road1_height&&robot_left<width) {
+            $(".robot").css("left",  $(".robot").offset().left + 10);}
 //////////up
-        if (e.keyCode == 38&&robot_top>walk4_top||e.keyCode == 38&&robot_top<=zone1_height&&robot_top>park1_top) {
+        if (e.keyCode == 38&&robot_top>road1_height&&(robot_left<=road2_left||robot_left>=road2_width)) {
             $(".robot").css("top",  $(".robot").offset().top - 10);}
-        if (e.keyCode == 38&&robot_top<=walk4_top&&robot_left>=cross1_1_left&&robot_left<=cross1_2_left&&robot_top>park1_top&&(green.css("background-color")=="rgb(43, 205, 30)"||green.css("background-color")=="rgb(175, 250, 154)")) {
-            $(".robot").css("top",  $(".robot").offset().top- 10);}
-        if (e.keyCode == 38&&robot_top< walk4_top&&robot_left>=cross1_1_left&&robot_left<=cross1_2_left&&robot_top>park1_top&&(green.css("background-color")!="rgb(43, 205, 30)"||green.css("background-color")!="rgb(175, 250, 154)")) {
-            $(".robot").css("top",  $(".robot").offset().top- 10);}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////||e.keyCode == 37&&robot_top<=cross2_1_top&&robot_top>=cross2_2_top&&robot_left<=walk2_left&&robot_left>=cross2_1_top
+        if (e.keyCode == 38&&robot_top<=road1_height&&((robot_left>=cross1_left&&robot_left<=cross1_width)||robot_left>=cross2_left&&robot_left<=cross2_width)&&robot_top>road1_top) {
+            $(".robot").css("top",  $(".robot").offset().top - 10);}
+        if (e.keyCode == 38&&robot_left>=road2_left&&robot_left<=road2_width&&(robot_top>cross3_top&&robot_top<=cross3_height||robot_top>cross4_top&&robot_top<=cross4_height)) {
+            $(".robot").css("top",  $(".robot").offset().top - 10);}
+        if (e.keyCode == 38&&robot_top<=road1_top&&(robot_left<=road2_left||robot_left>=road2_width)&&robot_top>table_top) {
+            $(".robot").css("top",  $(".robot").offset().top - 10);}
+
 //////////////left
-        if (e.keyCode == 37&&robot_left>park1_left&&robot_top<=zone1_height||e.keyCode == 37&&robot_left>park1_left&&robot_left<=zone1_width&&robot_top>=walk4_top) {
-           $(".robot").css("left",  $(".robot").offset().left -10);}
-        if (e.keyCode == 37&&robot_top>=zone1_height&&robot_top<=walk4_top&&robot_left>cross1_1_left&&robot_left<=cross1_2_left) {
-            $(".robot").css("left",  $(".robot").offset().left -10);}
 
-
+        if (e.keyCode == 37&&(robot_left<=road2_left||robot_left>road2_width)&&robot_top<=road1_top&&robot_left>table_left) {
+            $(".robot").css("left",  $(".robot").offset().left - 10);}
+        if (e.keyCode == 37&&robot_left>=road2_left&&robot_left<=road2_width&&(robot_top>=cross3_top&&robot_top<=cross3_height||robot_top>=cross4_top&&robot_top<=cross4_height)) {
+            $(".robot").css("left",  $(".robot").offset().left - 10);}
+        if (e.keyCode == 37&&robot_top<=road1_height&&((robot_left>cross1_left&&robot_left<=cross1_width)||robot_left>cross2_left&&robot_left<=cross2_width)&&robot_top>road1_top) {
+            $(".robot").css("left",  $(".robot").offset().left - 10);}
+        if (e.keyCode == 37&&(robot_left<=road2_left||robot_left>road2_width)&&robot_top>=road1_height&&robot_left>table_left) {
+            $(".robot").css("left",  $(".robot").offset().left - 10);}
     });
     sec1=10;
     sec2=2;
